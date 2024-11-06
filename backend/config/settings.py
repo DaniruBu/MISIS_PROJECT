a2ed04f4fa3583dbdbe66f55f32eed6b98fbcb7c
DaniruBu <buda-0000@mail.ru>
2024-11-06 22:11:54 +0300

    test

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
