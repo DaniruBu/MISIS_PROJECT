872f89d44200bdf0f93e016d81186d4e02f70595
DaniruBU <buda-0000@mail.ru>
2024-11-07 09:07:21 +0300

    test

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,8 +80,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
-    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+    ], 'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 # Password validation
