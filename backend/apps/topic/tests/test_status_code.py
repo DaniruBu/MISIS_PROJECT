032a45e7d32fa15e701b6083e226b65de490ae2a
DaniruBu <buda-0000@mail.ru>
2024-11-08 19:31:32 +0300

    что-то

--- backend/apps/topic/tests/test_status_code.py
+++ backend/apps/topic/tests/test_status_code.py
@@ -1,13 +1,9 @@
 from django.urls import reverse
-
 from rest_framework import status
 from rest_framework.test import APITestCase
-
 
 class StatusCodeTestCase(APITestCase):
     def test_create_account(self):
         url = reverse('topic-list')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-
