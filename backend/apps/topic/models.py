5715534c07c6dfaa7213e7e9b414e48fd3d33aab
DaniruBu <buda-0000@mail.ru>
2024-11-10 20:09:24 +0300

    pre-commit

--- backend/apps/topic/models.py
+++ backend/apps/topic/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Topic(models.Model):
     title = models.CharField(max_length=50, blank=True)
     description = models.TextField(max_length=100, blank=False)
